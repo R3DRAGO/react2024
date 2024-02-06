c12c753f6e09f4fff912d23d6f3914e6ec6c5e4f
bagil <bagilo.v@icloud.com>
2024-02-06 12:39:28 +0200

    lesson1

---
+++ src/components/PostContainer/Posts/Posts.js
@@ -0,0 +1,35 @@
+import {useEffect, useState} from "react";
+import {Post} from "../Post/Post";
+import axios from "axios";
+import {axiosService} from "../../../services/axiosService";
+import {urls} from "../../../constant/urls";
+import {postService} from "../../../services/postService";
+import {PostDetails} from "../PostDetails/PostDetails";
+import css from './Posts.module.css'
+
+const Posts = () => {
+    const [posts, setPosts] = useState([])
+    const [postDetails, setPostDetails] = useState(null);
+
+    useEffect(() => {
+        postService.getAll().then(({data}) => setPosts(data))
+
+    }, []);
+
+    const getCurrentPost = (post) =>{
+        setPostDetails(post)
+    }
+    return (
+        <div className={css.Posts}>
+            <div>
+                {posts.map(post => <Post key={post.id} post={post} getCurrentPost={getCurrentPost}/>)}
+            </div>
+
+            <hr/>
+                {postDetails && <PostDetails postDetails={postDetails}/>}
+
+        </div>
+    );
+};
+
+export {Posts};
---
+++ src/services/axiosService.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+import {baseURL} from "../constant/urls";
+
+const axiosService = axios.create({baseURL});
+
+export {
+    axiosService
+}
