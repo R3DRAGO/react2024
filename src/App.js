842a6a7235931932c36008b7bd5eb77bc3d299eb
bagil <bagilo.v@icloud.com>
2024-02-09 15:21:29 +0200

    lesson3/part1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import {useRef, useState} from "react";
+
 const App = () => {
+    // const [count, setCount] = useState(0)
+    const name = useRef();
+    const age = useRef();
+    const [user, setUser] = useState({})
+    const save = (e) => {
+        e.preventDefault()
+        // console.log(name.current.value);
+        // console.log(age.current.value);
+        console.log(e.target.name.value);
+        console.log(e.target.age.value);
+    }
+
+    console.log(user);
+    const handler = (e) =>{
+        e.preventDefault()
+        setUser(prevState => ({...prevState, [e.target.name]:e.target.value}))
+    }
     return (
         <div>
-            App
+            {/*<form onSubmit={save}>*/}
+            <form>
+                {/*<input type="text" placeholder={'name'} ref={name}/>*/}
+                {/*<input type="text" placeholder={'age'} ref={age}/>*/}
+                <input type="text" placeholder={'name'} name={'name'} onChange={handler}/>
+                <input type="text" placeholder={'age'} name={'age'} onChange={handler}/>
+                <button  onClick={save}>save</button>
+            </form>
+            {/*<div>count: {count}</div>*/}
+            {/*<button onClick={()=>setCount(prevState => prevState + 1)}>inc</button>*/}
         </div>
     );
 };
