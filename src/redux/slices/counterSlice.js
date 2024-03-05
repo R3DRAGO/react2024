02b6047dec487808fd16f10e15d5e2033e793872
bagil <bagilo.v@icloud.com>
2024-03-05 17:28:43 +0200

    lesson7/part1

---
+++ src/redux/slices/counterSlice.js
@@ -0,0 +1,35 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+    count: 0
+};
+
+const counterSlice = createSlice({
+    name: 'counterSlice',
+    initialState,
+    reducers: {
+        inc: state => {
+            state.count += 1
+        },
+        dec: state => {
+            state.count -= 1
+        },
+        reset: state => {
+            state.count = 0
+        },
+
+    }
+});
+
+const {reducer:counterReducer, actions} = counterSlice;
+
+const counterActions = {
+    ...actions
+}
+
+
+export {
+    counterReducer,
+    counterActions
+}
+
