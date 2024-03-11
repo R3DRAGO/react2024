4a2e3c9819c6a6883431866ba7f48bb136410f31
bagil <bagilo.v@icloud.com>
2024-03-11 17:36:56 +0200

    lesson8/hw

---
+++ src/constants/urls.js
@@ -0,0 +1,17 @@
+const baseURL = 'https://rickandmortyapi.com/api'
+
+const episodes = '/episode'
+
+const characters = '/character'
+
+const urls = {
+    episodes,
+    characters:{
+        byIds:(ids) => `${characters}/${ids}`
+    }
+}
+
+export {
+    urls,
+    baseURL
+}
